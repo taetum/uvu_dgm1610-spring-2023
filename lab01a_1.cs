9fdff79cf7a9f561102365296aaeecde7cf36ea7
Taetum <taetummote@gmail.com>
2023-01-16 18:35:04 -0700

    Lab01a_1 added
    first test. it keeps saying i need a static main but if i add it it invalidates my other main and causes more errors. not sure

---
+++ lab01a_1.cs
@@ -0,0 +1,33 @@
+﻿//the program keeps saying i don't have a static main method, but idk how to fix it
+using System;
+
+public class Program
+{
+    public Dagger thiefsDagger;
+    public Sword masterSword;
+
+    public void Main()
+    {
+        thiefsDagger = new Dagger();
+        masterSword = new Sword();
+        Console.WriteLine("You have dropped the " + thiefsDagger.daggerName + ".");
+        Console.WriteLine("You have picked up the " + masterSword.swordName + ". Your attack speed has increased from " + thiefsDagger.attackSpeed + " to " + masterSword.attackSpeed + ".");
+        Console.WriteLine("Use this sacred blade wisely.");
+    }
+
+    public class Sword
+    {
+        public string swordName = "Master Sword";
+        public int swordDamage = 10;
+        public float attackSpeed = 1.5f;
+
+    }
+
+    public class Dagger
+    {
+        public string daggerName = "Thief's Dagger";
+        public int daggerDamage = 2;
+        public float attackSpeed = 0.8f;
+
+    }
+}
