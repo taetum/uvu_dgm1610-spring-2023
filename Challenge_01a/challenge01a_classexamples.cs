8d0d2e7602d3e439898c7d4ab1e194ffd6770b53
Taetum <taetummote@gmail.com>
2023-01-20 14:30:13 -0700

    challenge01a

---
+++ Challenge_01a/challenge01a_classexamples.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Change "HW" to "Hello World"
+        Console.WriteLine("Hello World");
+    }
+}
---
+++ Challenge_01a/challenge01a_correcttheerrors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class ClassExample
+{
+
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+    public string userName = "Bob";
+    public bool isBobCool = true;
+    public float bobHeight = 6.3f;
+}
+
+public class ClassWithFunctions
+{
+    public void RunFunction()
+    {
+        Console.WriteLine("Running Function");
+        Console.WriteLine("Your user name is " + username + ".");
+        int x = 300 + 20
+
+        Console.WriteLine(x);
+        Console.WriteLine(number * bobHeight);
+    }
+}
