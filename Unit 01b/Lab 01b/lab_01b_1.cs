211aa7a258d862a0b346ca90fa2f298037704c1b
Taetum <taetummote@gmail.com>
2023-01-22 14:26:38 -0700

    lab_01b_1
    folders also added

---
+++ Unit 01b/Lab 01b/lab_01b_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public Holder classHolder;
+    public void Main()
+    {
+        classHolder = new Holder();
+        Console.WriteLine("Insert username.");
+        string username = Console.ReadLine();
+        classHolder.CheckUsername(username);
+
+    }
+
+}
+
+public class Holder
+{
+
+    public void CheckUsername(string username)
+    {
+        if (username == "tae")
+        {
+            Console.WriteLine("Awesome username. Very cool.");
+        }
+        else
+        {
+            Console.WriteLine("Terrible username. Very bad.");
+        }
+    }
+}
