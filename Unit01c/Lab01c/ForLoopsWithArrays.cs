8dd354dbf016fa83245c1734b8d7ae329ae6b568
Taetum <taetummote@gmail.com>
2023-01-30 17:11:20 -0700

    added lab01c forloops and while loops
    also changed folder names

---
+++ Unit01c/Lab01c/ForLoopsWithArrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] weapons = { "Sword", "Hammer", "Staff", "Wand", "Hands" };
+        string[] heroName = { "Fighter", "Barbarian", "Rogue", "Wizard" };
+        for (int i = 0; i < 1; i++)
+        {
+            Console.WriteLine("You have chosen to be a Fighter.");
+            Console.WriteLine("You have equipped a " + weapons[0] + ".");
+        }
+        for (int i = 1; i < 2; i++)
+        {
+            Console.WriteLine("You have chosen to be a Barbarian.");
+            Console.WriteLine("You have equipped a " + weapons[1] + ".");
+        }
+        for (int i = 2; i < 3; i++)
+        {
+            Console.WriteLine("You have chosen to be a Rogue.");
+            Console.WriteLine("You have equipped a " + weapons[2] + ".");
+        }
+        for (int i = 3; i < 4; i++)
+        {
+            Console.WriteLine("You have chosen to be a Wizard.");
+            Console.WriteLine("You have equipped a " + weapons[3] + ".");
+        }
+    }
+
+}
+
---
+++ Unit01c/Lab01c/whileloops1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+        Console.WriteLine("are you cool");
+        string isCool = Console.ReadLine();
+        while (isCool != "yes")
+        {
+            Console.WriteLine("you can't enter if you're not cool.");
+            Console.WriteLine("try again.");
+            return;
+        }
+        if (isCool == "yes")
+        {
+            Console.WriteLine("nice. you can come in");
+        }
+
+    }
+
+    public class CoolVariable
+    {
+        public string isCool;
+    }
+}
