58eb111a1d8a3c7c3632905aaf3a045dc3044981
Taetum <taetummote@gmail.com>
2023-02-01 13:51:36 -0700

    added for each loops w arrays

---
+++ Unit01c/Lab01c/ForEachLoopsWithArrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public void Main()
+    {
+        string[] owner = { "Farmer", "Pet Owner", "Cat Lady", "Rider", "Old Lady At Park" };
+        string[] animals = { "Cow", "Dog", "Cat", "Horse", "Duck" };
+
+        foreach (string i in owner)
+        {
+            Console.WriteLine(i + "'s");
+            for (int n = 0; n < 1; n++)
+            {
+
+                Console.WriteLine(animals[2]);
+            }
+
+
+            /*	foreach (string n in animals) {
+			Console.WriteLine(animals[3]);
+		} */
+
+        }
+
+    }
+}
+
